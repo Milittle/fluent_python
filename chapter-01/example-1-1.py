40a5daf1c4979ffa6e0d4088b95e243359a12178
milittle <mizeshuang@gmail.com>
2018-06-01 23:52:48 +0800

    add example-1-2 and chapter-01 README

--- chapter-01/example-1-1.py
+++ chapter-01/example-1-1.py
@@ -19,12 +19,51 @@ def test_namedtuple():
     card = Card('7', 'diamonds')
     print(card)
 
+    
+
 def test_FrenchDeck():
     deck = FrenchDeck()
     print(len(deck))
 
+    # first element
+    print(deck[0]) # use __getitem__ method implement
+
+    # last element
+    print(deck[-1]) # same basic
+
+def random_choice_deck():
+    from random import choice
+    deck = FrenchDeck()
+    print(choice(deck))
+
+def slice_deck():
+    deck = FrenchDeck()
+    print(deck[0:3]) # show deck form 0 to 3 indices
+    print(deck[12::13]) # show deck from 12 to the end with interval is 13
+
+def iterate_call():
+    deck = FrenchDeck()
+    for i in deck: # when we implement the __getitem__ method and it will make the deck can iteration
+        print(i)
+
+def reverse_call():
+    deck = FrenchDeck()
+    for i in reversed(deck):
+        print(i)
+
+
+def sort_deck():
+    suit_values = dict(spades=0, hearts=1, diamonds=2, clubs=4)
+    def spades_high(card):
+        rank_value = FrenchDeck.ranks.index(card.rank) # get cards every index for 0 to 12 and every four elements is one group
+        return rank_value * len(suit_values) + suit_values[card.suit]
+    deck = FrenchDeck()
+    for i in sorted(deck, key=spades_high):
+        print(i)
+
+
 def main():
-    test_FrenchDeck()
+    sort_deck()
 
 if __name__ == '__main__':
     main()
---
+++ chapter-01/example-1-2.py
@@ -0,0 +1,28 @@
+# A simple 2D vector class
+
+from math import hypot
+
+class Vecotr:
+    def __init__(self, x=0, y=0):
+        self.x=x
+        self.y=y
+    # when we call str(vector), if the __str__ method not implement then the interpretor will call __repr__
+    def __repr__(self): # it will be directly invoke the vector instance. 
+        return 'Vector(%r, %r)' % (self.x, self.y)
+    
+    def __abs__(self): # support abs method
+        return hypot(self.x, self.y)
+
+
+    # if the vector instance in the if-statement or something condition statement, the interpretor will call this method
+    # if this method not implement, the interpretor will call __len__, if len return 0, False, Otherwise, True
+    def __bool__(self): # bool method
+        return bool(abs(self))
+
+    def __add__(self, other): # plus
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar): # multiply
+        return Vector(self.x * scalar, self.y * scalar)
